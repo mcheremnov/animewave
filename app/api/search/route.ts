92171cc225d0cad40537fa88f71b997f77293534
mcheremnov <cheremnovmaksym@gmail.com>
2025-02-03 20:00:12 +0200

    Update linter and fix issues

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,3 +1,10 @@
-export async function GET(_: Request) {
-    return new Response('Hello from search route')
+export async function GET(request: Request) {
+    const { searchParams } = new URL(request.url);
+    const name = searchParams.get('name');
+    const getResponse = await fetch(
+        `${process.env.HIANIME}/api/v2/hianime/search?q=${name}&page=1`,
+    );
+    const data = await getResponse.json();
+
+    return data;
 }
