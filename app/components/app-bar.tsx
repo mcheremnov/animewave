361f5952eb43d5282919fb0c3017e7ee9e14caf9
mcheremnov <cheremnovmaksym@gmail.com>
2025-02-03 12:03:22 +0200

    Add app bar and first route

---
+++ app/components/app-bar.tsx
@@ -0,0 +1,26 @@
+import { Disclosure } from '@headlessui/react'
+import Image from 'next/image'
+import Link from 'next/link'
+import logo from '../../public/white_on_trans.png'
+
+export const AppBar = () => {
+    return (
+        <Disclosure as="nav" className="bg-gray-800">
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div className="relative flex h-16 items-center justify-between">
+                    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                        <div className="flex shrink-0 items-center">
+                            <Link href="/"><Image
+                                alt="AnimeWave"
+                                height={100}
+                                width={70}
+                                src={logo}
+                                className="w-auto"
+                            /></Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Disclosure>
+    )
+}
